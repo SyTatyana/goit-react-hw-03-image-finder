7497c564fea0722e8dd7ce68d32cb2cf18b3ceac
Tatyana Syrbu <tanja.karamilja@gmail.com>
2022-11-06 19:20:24 +0200

    fixed

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -8,7 +8,7 @@ export const Loader = () => {
         height="80"
         width="80"
         radius="9"
-        color="#3f51b5"
+        color="#008080"
         ariaLabel="three-dots-loading"
         wrapperStyle={{}}
         wrapperClassName=""
